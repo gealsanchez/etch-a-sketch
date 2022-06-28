fdafac7c6ab39220304ec3cba8ed741be08e50bb
gealsanchez <gealsanchez@gmail.com>
2022-06-28 12:03:00 -0500

    Almost done

--- eas_code.js
+++ eas_code.js
@@ -1,5 +1,54 @@
-for (let i = 0; i < (16 * 16); i++) {
+const resbut = document.createElement('button');
+resbut.textContent = 'RESET';
+
+const diva = document.createElement('div');
+const divb = document.createElement('div');
+
+divb.className = "griddiv";
+
+container.appendChild(diva);
+container.appendChild(divb);
+diva.appendChild(resbut);
+let col = 16;
+
+divb.style.setProperty('grid-template-columns', 'repeat(' + col + ', 1fr)');
+
+for (let i = 0; i < (col * col); i++) {
     const div = document.createElement('div');
+    div.className = "grid";
     div.textContent = i;
-    container.appendChild(div);
+    divb.appendChild(div);
 }
+
+function clear() {
+
+    const col = prompt('Please write the number of columns?');
+    divb.style.setProperty('grid-template-columns', 'repeat(' + col + ', 1fr)');
+
+    if (col == null || col == '') {
+        alert('Canceled!');
+    } else if (col > 100) alert('the number can not be greater than 100');
+
+    for (let i = 0; i < (col * col); i++) {
+        const div = document.createElement('div');
+        div.className = "grid";
+        div.textContent = i;
+        divb.appendChild(div);
+    }
+
+    divs.forEach(div => div.addEventListener('mouseover', function (event) {
+        event.target.style.backgroundColor = "orange";
+        event.target.style.color = "orange";
+    }));
+
+}
+
+resbut.addEventListener('click', clear);
+
+const divs = document.querySelectorAll('.grid');
+
+divs.forEach(div => div.addEventListener('mouseover', function (event) {
+    event.target.style.backgroundColor = "orange";
+    event.target.style.color = "orange";
+}));
+
